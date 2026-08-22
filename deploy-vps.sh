94654b2285ca9f51b3a16a736370957cc9f2ef09
gitchx <soma.ksg@gmail.com>
2026-08-23 04:50:36 +0900

    Deploy to HTTPS document root

--- deploy-vps.sh
+++ deploy-vps.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 project_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
-web_root="/var/www/html"
+web_root="/var/www/wavbits.com"
 vhost_file="/etc/httpd/conf.d/wavbits.com.conf"
 
 install -o root -g root -m 0644 "$project_dir/index.html" "$web_root/index.html"
@@ -11,9 +11,9 @@ install -o root -g root -m 0644 "$project_dir/style.css" "$web_root/style.css"
 cat >"$vhost_file" <<'APACHE'
 <VirtualHost *:80>
     ServerName wavbits.com
-    DocumentRoot /var/www/html
+    DocumentRoot /var/www/wavbits.com
 
-    <Directory /var/www/html>
+    <Directory /var/www/wavbits.com>
         Options FollowSymLinks
         AllowOverride None
         Require all granted
